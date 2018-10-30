f2c1ea9a74561d6a04c0294a0def9e66b3398275
Thibault BURGER <thibault.burger@ihu-strasbourg.eu>
2018-10-30 14:21:13 +0100

    feat: add coucou.java

---
+++ coucou.java
@@ -0,0 +1,9 @@
+public class coucou {
+  public static void main(String[] args) {
+    System.out.println(coucou.getCoucou());
+  }
+  
+  public static String getCoucou() {
+    return "coucou";
+  }
+}
